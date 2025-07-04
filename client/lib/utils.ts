a0819a3a77c8b0085fa469cb9bce02eb93661e96
Václav Martinovský <19268476+vaclove@users.noreply.github.com>
2025-07-04 15:40:58 +0200

    fixed api url

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -6,6 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getApiUrl(path: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/';
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://guests.irmf.cz/api';
   return `${baseUrl}${path}`;
 }
